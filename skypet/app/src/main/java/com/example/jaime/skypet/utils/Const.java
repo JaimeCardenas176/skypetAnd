cfa46fd95a8ac92fcf847fc7df76a822e974f18e
jaimeCardenas176 <jaime.cardenas176gmail.com>
2018-06-16 17:41:17 +0200

    solucionados los problemas de android estudio.. al fin funciona..

---
+++ skypet/app/src/main/java/com/example/jaime/skypet/utils/Const.java
@@ -0,0 +1,15 @@
+package com.example.jaime.skypet.utils;
+
+
+public class Const {
+    //public static final String BASE_URL = "https://sevisafeapi-iynazvuerb.now.sh/";
+    public static final String BASE_URL = "http://localhost:33000/";
+    public static final String USER_NAME = "nombre_usuario";
+    public static final String USER_ID = "id_usuario";
+    public static final String USER_SURNAME = "apellidos_usuario";
+    public static final String USER_EMAIL = "email_usuario";
+    public static final String USER_ADDRESS = "direccion_usuario";
+    public static final String USER_PHONE = "telefono_usuario";
+    public static final String USER_ADMIN = "condicion_usuario";
+    public static final String USER_TOKEN = "token_usuario";
+}
